f668f2d2995373e1395629d4286be277347ed6e1
Satyen Verma <sverma44@asu.edu>
2020-08-16 17:05:55 -0700

    encryption of password

--- src/models/user.js
+++ src/models/user.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -14,7 +15,7 @@ const User = mongoose.model('User', {
         lowercase: true,
         validate(value) {
             if (!validator.isEmail(value)) {
-                throw new Error('Email is not valid')
+                throw new Error('Email is invalid')
             }
         }
     },
@@ -33,11 +34,23 @@ const User = mongoose.model('User', {
         type: Number,
         default: 0,
         validate(value) {
-            if (value<0) {
-                throw new Error('Age must be positive!')
+            if (value < 0) {
+                throw new Error('Age must be a postive number')
             }
         }
     }
 })
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
