bcdf9888da297cb44ff90e88118f052bc16846b7
Satyen Verma <sverma44@asu.edu>
2020-08-18 17:33:00 -0700

    Added user authentication to GET user/me endpoint

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//     res.send('Site under maintenance!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
